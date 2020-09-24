ff8481bd074e99d727e827154376da81d0cc9ccb
captainxavier <osisxavier@gmail.com>
2020-09-24 11:28:28 +0300

    initial commit

---
+++ src/main/java/com/xavier/jokeapp/controllers/JokeController.java
@@ -0,0 +1,26 @@
+package com.xavier.jokeapp.controllers;
+
+import com.xavier.jokeapp.services.JokeService;
+import com.xavier.jokeapp.services.JokeServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+
+    private JokeService jokeService;
+
+    @Autowired
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model){
+        model.addAttribute("joke",jokeService.getJoke());
+
+        return "chucknorris";
+    }
+}
---
+++ src/main/java/com/xavier/jokeapp/services/JokeService.java
@@ -0,0 +1,6 @@
+package com.xavier.jokeapp.services;
+
+public interface JokeService {
+
+    String getJoke();
+}
